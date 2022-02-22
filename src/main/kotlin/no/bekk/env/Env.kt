8a76e67bded32ddfc1082b5c910fc4df3441fa30
Dragutin Vujovic <dragutin.vujovic@nav.no>
2022-02-22 13:08:15 +0100

    Update environment variables

--- src/main/kotlin/no/bekk/env/Env.kt
+++ src/main/kotlin/no/bekk/env/Env.kt
@@ -1,8 +1,8 @@
 package no.bekk.env
 
-val DB_USERNAME: String = System.getenv("db_USERNAME")
-val DB_PASSWORD: String = System.getenv("db_PASSWORD")
-val DB_HOST: String = System.getenv("db_HOST")
-val DB_DATABASE: String = System.getenv("db_DATABASE")
-val DB_PORT: Int = System.getenv("db_PORT").toInt()
+val DB_USERNAME: String = System.getenv("DB_USERNAME")
+val DB_PASSWORD: String = System.getenv("DB_PASSWORD")
+val DB_HOST: String = System.getenv("DB_HOST")
+val DB_DATABASE: String = System.getenv("DB_DATABASE")
+val DB_PORT: Int = System.getenv("DB_PORT").toInt()
 val SANITY_PROJECT_ID: String = "li581mqu"
