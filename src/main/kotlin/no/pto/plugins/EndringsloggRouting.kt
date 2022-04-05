44fed1bed24b3b6180d04aefb10c3e4c0e122e85
stianStensli <stian.horn.stensli@nav.no>
2022-04-05 19:54:51 +0200

    la til mer logging

--- src/main/kotlin/no/pto/plugins/EndringsloggRouting.kt
+++ src/main/kotlin/no/pto/plugins/EndringsloggRouting.kt
@@ -31,8 +31,10 @@ fun Application.configureEndringsloggRouting(client: SanityClient) {
                 when (val endringslogger = client.queryEndringslogg(endringsloggPoaoqQuery)) {
                     is Ok -> {
                         if (endringslogger.value.result.isEmpty()) {
+                            logger.info("status=204, method=GET, /endringslogg, fant ingen endringslogger")
                             call.response.status(HttpStatusCode(204, "Data for app $appId doesn't exist."))
                         } else {
+                            logger.info("status=200, method=GET, /endringslogg, {} endringslogger.", endringslogger.value.result.size)
                             call.respond(endringslogger.value.result.map {
                                 it.copy(
                                     seen = it.id in seenEntryIds,
