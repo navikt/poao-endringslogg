85c85a445e7854c82b93a74f5ce07ea234058a70
Dragutin Vujovic <dragutin.vujovic@nav.no>
2023-09-04 15:32:35 +0200

    Update api version

--- src/main/kotlin/no/pto/env/Env.kt
+++ src/main/kotlin/no/pto/env/Env.kt
@@ -9,7 +9,7 @@ val DB_HOST: String = System.getenv("DB_HOST") ?: ""
 val DB_DATABASE: String = System.getenv("DB_DATABASE") ?: ""
 val DB_PORT: Int = System.getenv("DB_PORT")?.toInt() ?: 5432
 val SANITY_PROJECT_ID: String = "li581mqu"
-val API_VERSION_ENDRINGSLOGG: String = "v2021-06-07"
+val API_VERSION_ENDRINGSLOGG: String = "v2021-10-21"
 
 private val systemmeldingPoaoQ1Query = URLEncoder.encode("*[_type=='alert_overiskten'][publisert_q1]", Charset.forName("utf-8"))
 private val systemmeldingPoaoProdQuery = URLEncoder.encode("*[_type=='alert_overiskten'][publisert]", Charset.forName("utf-8"))
