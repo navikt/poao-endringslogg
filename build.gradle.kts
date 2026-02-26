454ea2bcce8574202ca73bd4c921725eb8dccd67
Mathias S. Müller <mathias.sundt.muller@nav.no>
2026-02-26 14:53:09 +0100

    Oppgrader HikariCP til v7

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
     implementation("org.flywaydb:flyway-database-postgresql:12.0.1")
     implementation("com.github.ben-manes.caffeine:caffeine:3.2.0")
     implementation("com.launchdarkly:okhttp-eventsource:4.1.1")
-    implementation("com.zaxxer:HikariCP:6.2.1")
+    implementation("com.zaxxer:HikariCP:7.0.2")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
 }
